3ab2c4d40f12ffac1513a86c1a5bba13833e3632
Juri Matsuda <matsuda@ecgain.com>
2021-05-09 16:38:05 +0900

    first commit

---
+++ pages/users/_id.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <p>
+      USER ID: {{userId}}
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      userId: this.$route.params.id
+    }
+  },
+  name: "_id"
+}
+</script>
+
+<style scoped>
+
+</style>
